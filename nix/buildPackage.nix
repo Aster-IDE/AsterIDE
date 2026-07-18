1bcb08455de974d3854c93b73ba4dadb9bba197b
Invra <identificationsucks@gmail.com>
2026-07-18 16:39:54 +1000

    chore: remove source filtering

--- nix/buildPackage.nix
+++ nix/buildPackage.nix
@@ -40,7 +40,7 @@ let
   commonArgs = {
     pname = "asteride";
     version = "2.0.0";
-    src = craneLib.cleanCargoSource ../.;
+    src = craneLib.path ../.;
     strictDeps = true;
     nativeBuildInputs = buildDeps;
     buildInputs = buildDeps;
